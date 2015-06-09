17f343af02b0ba55030e59d65682166719affb55
Jasper Bok <hello@jasperbok.nl>
2015-06-09 19:52:26 +0200

    Created a parser

---
+++ art/parser.js
@@ -0,0 +1,40 @@
+// Matches lines like 'a=#ff0000' which are treated as color settings.
+var regex = /^(.{1})=(#[0-9a-fA-F]{3,6})$/g;
+
+function parse(text) {
+  var parts = text.split('\n');
+  var colors = {};
+  var document = [];
+  var previousLineType = '';
+
+  // If the last line is empty, remove it.
+  if (!parts[parts.length - 1].replace('\n', '')) {
+    parts = parts.slice(0, parts.length -1);
+  }
+
+  parts.forEach(function(p, i) {
+    var result = regex.exec(p);
+
+    // This is a color configuration line.
+    if (result) {
+      previousLineType = 'color';
+      colors[result[1]] = result[2];
+    }
+    // This is padding.
+    else if (!p.trim() && previousLineType === 'color') {
+      previousLineType = 'whitespace';
+    }
+    // This is content.
+    else {
+      document.push(p);
+      previousLineType = 'content';
+    }
+  });
+
+  return {
+    colors: colors,
+    document: document
+  };
+}
+
+module.exports = parse;
